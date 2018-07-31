f01dfd5dfbe91139db0f2b06c772834511d05647
punam96 <32470381+punam96@users.noreply.github.com>
2018-08-01 02:09:19 +0530

    Add files via upload

---
+++ Revision90/Revision90/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Revision90
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtboxpath = new System.Windows.Forms.TextBox();
+            this.browsebtn = new System.Windows.Forms.Button();
+            this.calculatebtn = new System.Windows.Forms.Button();
+            this.csvgenerator = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtboxpath
+            // 
+            this.txtboxpath.Location = new System.Drawing.Point(97, 36);
+            this.txtboxpath.Name = "txtboxpath";
+            this.txtboxpath.Size = new System.Drawing.Size(893, 26);
+            this.txtboxpath.TabIndex = 0;
+            // 
+            // browsebtn
+            // 
+            this.browsebtn.Location = new System.Drawing.Point(996, 30);
+            this.browsebtn.Name = "browsebtn";
+            this.browsebtn.Size = new System.Drawing.Size(100, 39);
+            this.browsebtn.TabIndex = 1;
+            this.browsebtn.Text = "Browse";
+            this.browsebtn.UseVisualStyleBackColor = true;
+            this.browsebtn.Click += new System.EventHandler(this.browsebtn_Click);
+            // 
+            // calculatebtn
+            // 
+            this.calculatebtn.Location = new System.Drawing.Point(443, 86);
+            this.calculatebtn.Name = "calculatebtn";
+            this.calculatebtn.Size = new System.Drawing.Size(312, 56);
+            this.calculatebtn.TabIndex = 5;
+            this.calculatebtn.Text = "Calculate";
+            this.calculatebtn.UseVisualStyleBackColor = true;
+            this.calculatebtn.Click += new System.EventHandler(this.calculatebtn_Click);
+            // 
+            // csvgenerator
+            // 
+            this.csvgenerator.Location = new System.Drawing.Point(430, 160);
+            this.csvgenerator.Name = "csvgenerator";
+            this.csvgenerator.Size = new System.Drawing.Size(344, 67);
+            this.csvgenerator.TabIndex = 6;
+            this.csvgenerator.Text = "Generate csv";
+            this.csvgenerator.UseVisualStyleBackColor = true;
+            this.csvgenerator.Click += new System.EventHandler(this.csvgenerator_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1360, 499);
+            this.Controls.Add(this.csvgenerator);
+            this.Controls.Add(this.calculatebtn);
+            this.Controls.Add(this.browsebtn);
+            this.Controls.Add(this.txtboxpath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtboxpath;
+        private System.Windows.Forms.Button browsebtn;
+        private System.Windows.Forms.Button calculatebtn;
+        private System.Windows.Forms.Button csvgenerator;
+    }
+}
+
---
+++ Revision90/Revision90/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Revision90
+{
+    public partial class Form1 : Form
+    {
+        DataTable datatable = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browsebtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select .CSV ffile";
+            ofd.ShowDialog();
+            txtboxpath.Text = ofd.FileName;
+        }
+
+        private void displaybtn_Click(object sender, EventArgs e)
+        {
+            string filePath = txtboxpath.Text;
+            StreamReader sr = new StreamReader(filePath);
+            string line = sr.ReadLine();
+            string[] value = line.Split(',');
+
+            DataRow row;
+            foreach (string dc in value)
+            {
+                datatable.Columns.Add(new DataColumn(dc));
+            }
+            while (!sr.EndOfStream)
+            {
+                value = sr.ReadLine().Split(',');
+                if (value.Length == datatable.Columns.Count)
+                {
+                    row = datatable.NewRow();
+                    row.ItemArray = value;
+                    datatable.Rows.Add(row);
+                }
+            }
+            
+        }
+
+        private void calculatebtn_Click(object sender, EventArgs e)
+        {
+            string filePath = txtboxpath.Text;
+            StreamReader sr = new StreamReader(filePath);
+            string line = sr.ReadLine();
+            string[] value = line.Split(',');
+
+            DataRow row;
+            foreach (string dc in value)
+            {
+                datatable.Columns.Add(new DataColumn(dc));
+            }
+            while (!sr.EndOfStream)
+            {
+                value = sr.ReadLine().Split(',');
+                if (value.Length == datatable.Columns.Count)
+                {
+                    row = datatable.NewRow();
+                    row.ItemArray = value;
+                    datatable.Rows.Add(row);
+                }
+            }
+
+
+            int startIndex = 0;
+            while (startIndex < datatable.Rows.Count)
+            {
+                DateTime timeStart = DateTime.ParseExact(datatable.Rows[startIndex][1].ToString(), "dd-MM-yy HH:mm",
+                    CultureInfo.InvariantCulture);
+                DateTime timeEnd = timeStart;
+                int lowestDValue = int.Parse(datatable.Rows[startIndex][3].ToString());
+                int lowestCValue = int.Parse(datatable.Rows[startIndex][2].ToString());
+                int lowestEValue = int.Parse(datatable.Rows[startIndex][4].ToString());
+                int endIndex = startIndex;
+                int newFValue = 0;
+                //loop to find end index of 5 minute loop
+                for (int j = startIndex++; j < datatable.Rows.Count; j++)
+                {
+                    endIndex = j;
+
+                    timeEnd = DateTime.ParseExact(datatable.Rows[endIndex][1].ToString(), "dd-MM-yy HH:mm",
+                        CultureInfo.InvariantCulture);
+                    if ((timeEnd - timeStart).TotalMinutes >= 5)
+                    {
+                        break;
+                    }
+                    int newDValue = int.Parse(datatable.Rows[endIndex][3].ToString());
+                    int newEValue = int.Parse(datatable.Rows[endIndex][4].ToString());
+                    newFValue = int.Parse(datatable.Rows[endIndex][5].ToString());
+                    if (newDValue < lowestDValue)
+                    {
+                        lowestDValue = newDValue;
+                    }
+                    if (newEValue > lowestEValue)
+                    {
+                        lowestEValue = newEValue;
+                    }
+                }
+                datatable.Rows[startIndex - 1][10] = lowestCValue.ToString();
+                datatable.Rows[startIndex - 1][11] = lowestDValue.ToString();
+                datatable.Rows[startIndex - 1][12] = lowestEValue.ToString();
+                datatable.Rows[startIndex - 1][13] = newFValue.ToString();
+                //loop from start to end to set lowest value
+
+
+
+                startIndex = endIndex;
+                if (startIndex >= datatable.Rows.Count - 1)
+                    break;
+            }
+
+           
+        }
+
+        private void csvgenerator_Click(object sender, EventArgs e)
+        {
+            datatable.ToCSV("result.CSV");
+            MessageBox.Show("Converted successfully to CSV file");
+        }
+
+       
+    }
+}
